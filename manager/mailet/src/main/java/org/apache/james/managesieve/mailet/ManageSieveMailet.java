ae4d04afe69ec8e3ea4b3d7c858517d05af4d0a0
Antoine Duprat <aduprat@apache.org>
2015-10-20 08:10:55 +0000

    JSIEVE-102 Adding injects
    git-svn-id: https://svn.apache.org/repos/asf/james/jsieve/trunk@1709532 13f79535-47bb-0310-9956-ffa450edef68

--- manager/mailet/src/main/java/org/apache/james/managesieve/mailet/ManageSieveMailet.java
+++ manager/mailet/src/main/java/org/apache/james/managesieve/mailet/ManageSieveMailet.java
@@ -26,7 +26,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
-import javax.annotation.Resource;
+import javax.inject.Inject;
 import javax.mail.MessagingException;
 
 import org.apache.james.managesieve.api.SieveParser;
@@ -246,7 +246,7 @@ public class ManageSieveMailet extends GenericMailet implements HelpProvider {
 
     /**
      */
-    @Resource(name = "sieverepository")
+    @Inject
     public void setSieveRepository(SieveRepository repository) {
         _sieveRepository = repository;
     }
@@ -255,7 +255,7 @@ public class ManageSieveMailet extends GenericMailet implements HelpProvider {
      * @param sieveParser
      *            the sieveParser to set
      */
-    @Resource(name = "sieveparser")
+    @Inject
     public void setSieveParser(SieveParser sieveParser) {
         _sieveParser = sieveParser;
     }
